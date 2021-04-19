3214c4f8d7eb6834767591dac350cceb6be0284b
Tiago Neves <rykarumstuler@gmail.com>
2021-04-19 13:57:11 -0300

    NLW 05 Node

---
+++ NODE/PROJECT/src/server.ts
@@ -0,0 +1,19 @@
+import express, { response } from 'express'
+
+const app = express()
+
+app.get("/", (req, res) => {
+    return res.json({
+        message: "Olá NLW 05"
+    })
+})
+
+app.post("/users", (req, res) => {
+    return res.json({
+        message: "Usuário salvo com sucesso!"
+    })
+})
+
+app.listen(3333, () => {
+    console.log("Server is running on port 3333")
+})
